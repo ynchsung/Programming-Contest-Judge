4e3232eaad4ec577dceebe49d2402d637cac2856
Tenyoku <sh961014@gmail.com>
2015-12-31 02:03:56 +0800

    Controller: timer

---
+++ src/main/java/Controller/ContestTimer.java
@@ -0,0 +1,86 @@
+package Controller;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by tenyoku on 2015/12/31.
+ */
+public class ContestTimer {
+    private int totalSecond;
+    private int secondCounted;
+    private Timer timer;
+    private final Object lock;
+    private Runnable callback;
+    private class CountDownTask extends TimerTask {
+        @Override
+        public void run() {
+            secondCounted += 1;
+            if (secondCounted >= totalSecond) {
+                synchronized (lock) {
+                    timer.cancel();
+                    callback.run();
+                }
+            }
+        }
+    }
+
+    public ContestTimer(int totalSecond, Runnable callback) {
+        this.totalSecond = totalSecond;
+        this.callback = callback;
+        this.timer = new Timer();
+        this.lock = new Object();
+    }
+
+    // must be called after got the lock
+    private void startCount() {
+        timer = new Timer();
+        timer.schedule(new CountDownTask(), 1000, 1000);
+    }
+
+    synchronized public void start() {
+        synchronized (lock) {
+            timer.cancel();
+            secondCounted = 0;
+            startCount();
+        }
+    }
+
+    public void pause() {
+        synchronized (lock) {
+            timer.cancel();
+        }
+    }
+
+    public void resume() {
+        synchronized (lock) {
+            timer.cancel();
+            startCount();
+        }
+    }
+
+    public void reset() {
+        synchronized (lock) {
+            timer.cancel();
+            secondCounted = 0;
+        }
+    }
+
+    public void setDuration(int duration) {
+        synchronized (lock) {
+            this.totalSecond = duration;
+        }
+    }
+
+    public int getDuration() {
+        return this.totalSecond;
+    }
+
+    public int getRemainTime() {
+        return Math.max(this.totalSecond-this.secondCounted, 0);
+    }
+
+    public int getCountedTime() {
+        return this.secondCounted;
+    }
+}
